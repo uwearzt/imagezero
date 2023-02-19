d226210f6c13a077aefe71dca3fb9cace783a3fd
Uwe Arzt <mail@uwe-arzt.de>
2023-02-19 22:26:58 +0100

    move to Codeberg

--- imagezero-rs/src/lib.rs
+++
@@ -1,9 +0,0 @@
-// ------------------------------------------------------------------------------
-// Copyright 2021 Uwe Arzt, mail@uwe-arzt.de
-// SPDX-License-Identifier: Apache-2.0
-// ------------------------------------------------------------------------------
-
-#![crate_name = "imagezero"]
-#![crate_type = "lib"]
-
-pub mod compress;
--- imagezero-sys/wrapper.hpp
+++
@@ -1,8 +0,0 @@
-// ------------------------------------------------------------------------------
-// Copyright 2021 Uwe Arzt, mail@uwe-arzt.de
-// SPDX-License-Identifier: Apache-2.0
-// ------------------------------------------------------------------------------
-
-#include <imagezero/libiz.h>
-#include <imagezero/image.h>
-// #include <imagezero/portableimage.h>
